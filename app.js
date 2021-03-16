18f8401681a1cfdca0f79ddbf2fc0d2e9a85c7ad
tekuto1437 <denisjerosenko79@gmail.com>
2021-03-16 14:14:08 +0200

    Save12

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
-onst listItems = document.querySelectorAll('#book-list ul li');
+const list = document.querySelector('#book-list ul');
 
-Array.from(listItems).forEach(function(item){
-  item.addEventListener('click', (e) => {
-
+list.addEventListener('click', (e) => {
+  if(e.target.className == 'delete'){
     const li = e.target.parentElement;
-    console.log('child element to remove:', li);
-    console.log('parent element to remove child from:', li.parentElement);
     li.parentNode.removeChild(li);
-
-  });
-});
-
-
-const link = document.querySelector('#page-banner a');
-
-link.addEventListener('click', function(e){
-  e.preventDefault();
-  console.log('Navigation to', e.target.textContent, 'was prevented');
+  }
 });
